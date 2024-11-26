00e629d9f0cb69d116ec4c0c036540fd46d683f1
Kiss Máté Tamás <matekiss343@gmail.com>
2024-11-26 15:58:48 +0100

    using gh

--- NixOS/system/matevono/home/shell/git.nix
+++ NixOS/system/matevono/home/shell/git.nix
@@ -3,7 +3,11 @@
     enable = true;
     userName = "...";
     userEmail = "...";
+    extraConfig = {
+      credential.helper = "gh";
+    };
   };
+  
   #programs.git-credential-oauth.enable = true;
   programs.gh.enable = true;
 }
