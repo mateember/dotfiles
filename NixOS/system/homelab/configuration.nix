be69da1fe8e1d9fa79cb6abb642c2879e5849f6d
Kiss Máté Tamás <matekiss343@gmail.com>
2025-01-22 21:24:54 +0100

    enabled firewall

--- NixOS/system/homelab/configuration.nix
+++ NixOS/system/homelab/configuration.nix
@@ -48,7 +48,16 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager.enable = true;
+
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [];
+      allowedUDPPortRanges = [
+      ];
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Budapest";
@@ -135,6 +144,7 @@
   };
   services = {
     tailscale.enable = true;
+    vscode-server.enable = true;
 
     jellyfin = {
       enable = true;
@@ -146,7 +156,7 @@
       enable = false;
       package = pkgs.nextcloud30;
       hostName = "hl.kmate.org";
-        config.adminpassFile = "/etc/nextcloud-admin-pass";
+      config.adminpassFile = "/etc/nextcloud-admin-pass";
       database.createLocally = true;
       https = true;
     };
@@ -193,22 +203,5 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  #  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 }
