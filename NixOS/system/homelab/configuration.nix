4c5bd6e9aace5372d5e020a7c21afb6fb5d30bd2
Kiss Máté Tamás <matekiss343@gmail.com>
2025-01-22 14:33:31 +0100

    bruuh

--- NixOS/system/homelab/configuration.nix
+++ NixOS/system/homelab/configuration.nix
@@ -118,7 +118,7 @@
     "set_fb_blank_1" = {
     description = "Set framebuffer blank state to 1";
     after = [ "multi-user.target" ];
-    execStart = "/bin/sh -c \"echo 1 > /sys/class/graphics/fb0/blank\"";
+    script = "/bin/sh -c \"echo 1 > /sys/class/graphics/fb0/blank\"";
     restart = true;
   };
     };
