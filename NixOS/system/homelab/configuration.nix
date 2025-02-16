1a1900d49e37b15743329d4421152673faddbdf7
Kiss Máté Tamás <matekiss343@gmail.com>
2025-02-16 21:11:15 +0100

    samba share

--- NixOS/system/homelab/configuration.nix
+++ NixOS/system/homelab/configuration.nix
@@ -55,7 +55,7 @@
 
     firewall = {
       enable = true;
-
+      allowPing = true;
       trustedInterfaces = [];
       allowedTCPPorts = [443 61208];
       allowedUDPPorts = [];
@@ -266,6 +266,38 @@
       };
     };
 
+    samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          "workgroup" = "WORKGROUP";
+          "server string" = "Home Samba Share";
+          "netbios name" = "HOMELAB";
+          "security" = "user";
+          #"use sendfile" = "yes";
+          #"max protocol" = "smb2";
+          # note: localhost is the ipv6 localhost ::1
+          "hosts allow" = "192.168.0. 192.168.1. 127.0.0.1 localhost";
+          "hosts deny" = "0.0.0.0/0";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
+        "public" = {
+          "path" = "/mnt/khdd/kshare";
+          "browseable" = "yes";
+          "read only" = "no";
+          "guest ok" = "yes";
+	  "guest only" = "yes";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+        };
+      };
+    };
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
     ddclient.enable = true;
     ddclient.configFile = "/home/mate/.local/ddclient/ddclient.conf";
 
